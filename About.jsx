bc2b51847c59ebb8decdae9365bfcadb8d0becd6
princerajsingh4002 <rajsinghprince20048103@gmail.com>
2026-03-07 20:58:32 +0530

    Add files via upload

---
+++ About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1>About Cab Booking System</h1>
+      <p>This project allows users to book cabs online easily.</p>
+    </div>
+  );
+};
+
+export default About;
